330a7ae7c4904d6240ddd8575e05460bbb94f441
Dana <User@DANAAWWAD>
2026-09-05 01:16:36 +0300

    Project

--- Employee & Leave Management/Models/Employees.cs
+++ Employee & Leave Management/Models/Employees.cs
@@ -9,7 +9,7 @@ namespace Employee___Leave_Management.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
-
+        
         public int RemainingLeaveDays { get; set; }
 
         public bool IsDeleted { get; set; }
